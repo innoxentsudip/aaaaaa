cc5fac13e30d5116b4602c35a6b33a92768ba010
Sudip kuikel <sudip.kuikel@gmail.com>
2016-10-21 12:04:50 +0545

    to find largest numbers using nested else if

--- largestnumbers.c
+++ largestnumbers.c
@@ -14,17 +14,27 @@ Date:2016-10-20
 int a,b,c;
 printf("Enter three numbers A B C");
 scanf("%d   %d   %d",&a,&b,&c);
-if (a>=b && a>=c)
-	{
-printf("%d is largest",a);
-	}
-else if (b>=a && b>=c)
-	{
-printf("\n%d is largest",b);
-	}
-else 
-	{
-printf("\n%d is greatest",c);
-	}
+if (a==b){
+	if (a>c){
+		printf("%d is greatest",a);
+}
+	else if (b==c){
+		if (b>a)
+			printf("%d  is greatest",b);
+}
+else if (c==a){
+	if (c>b)
+		printf("%d is greatest",c);
+}
+}
+else if (a>b&&a>c)
+printf("%d is greater",a);
+else if (b>c&&b>a)
+printf("%d is greater",b);
+else if (c>a&&c>b)
+printf("%d is greater",c);
 return 0;
 }
+	
+
+
