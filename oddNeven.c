4ee15bb259a7e0a731e07b7de12930c293a5f802
Sudip kuikel <sudip.kuikel@gmail.com>
2016-10-21 11:10:45 +0545

    Program to find odd or even number

---
+++ oddNeven.c
@@ -0,0 +1,26 @@
+/*
+Student name:Sudip Kuikel
+Subject:
+Roll no.:
+Lab no.:
+Program:
+Date:2016-10-20
+*/
+#include<stdio.h>
+
+
+     int main(){
+		 
+int a;
+printf("Enter a number to find out odd or even number:");
+scanf("%d",&a);
+if (a%2==0)
+{
+	printf("%d is a even number",a);
+}
+else 
+{
+	printf("%d is an odd number",a);
+}
+return 0;
+}
