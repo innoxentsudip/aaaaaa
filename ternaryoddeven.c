29819fef843705ac58af0bee471bece13041ee44
Sudip kuikel <sudip.kuikel@gmail.com>
2016-10-24 11:11:06 +0545

    Program to show oodd or even using ternary operators

---
+++ ternaryoddeven.c
@@ -0,0 +1,18 @@
+/*
+Student name:Sudip Kuikel
+Subject:
+Roll no.:
+Lab no.:
+Program:
+Date:2016-10-20
+*/
+#include<stdio.h>
+int  main()
+{
+int a;
+printf("Enter any number to decide odd or even:");
+scanf("%d",&a);
+( a%2==0 )? printf("%d is an even number",a):printf("%d is an odd number",a);
+return 0;
+}
+
