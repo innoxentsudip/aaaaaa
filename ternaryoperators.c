a711b8a26c60581ceb54600738b9875f9eec587e
Sudip kuikel <sudip.kuikel@gmail.com>
2016-10-24 11:12:25 +0545

    largest n smallest using ternary operators

---
+++ ternaryoperators.c
@@ -0,0 +1,18 @@
+/*
+Student name:Sudip Kuikel
+Subject:
+Roll no.:
+Lab no.:
+Program:
+Date:2016-10-20
+*/
+#include<stdio.h>
+int main (){
+int x,y;
+printf("Enter the value of X and Y respectively =");
+scanf("%d %d",&x,&y);
+	(x>y)?printf("largest is %d",x):printf("smallest is %d",y);
+	(x<y)?printf("\nlarges is %d",y):printf("\nsmallest is %d",x);	
+
+return 0;
+}
