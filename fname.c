b912f81b9371a53fdfc2ba61bb2075df41d2800a
Sudip kuikel <sudip.kuikel@gmail.com>
2016-11-15 10:17:36 +0545

    To print name 10 times

---
+++ fname.c
@@ -0,0 +1,18 @@
+/*
+Student Name:Sudip Kuikel
+Subject:BCS (Hons.)
+Roll No.:23
+Lab no:13
+Program:To print your name 10 times
+*/
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int i;
+for(i=0;i<=10;i++)
+{
+printf("\t \t My name is Sudip Kuikel.\b \n \n");
+}
+getch();
+}
